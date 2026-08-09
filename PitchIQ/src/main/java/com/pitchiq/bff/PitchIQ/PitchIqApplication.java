9b7f3698c9f7b15c655c8c7695ae3226f39dd889
Pellegrino Testa <82523501+pellegrinotesta@users.noreply.github.com>
2026-08-09 17:45:57 +0200

    Add files via upload

---
+++ PitchIQ/src/main/java/com/pitchiq/bff/PitchIQ/PitchIqApplication.java
@@ -0,0 +1,13 @@
+package com.pitchiq.bff.PitchIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchIqApplication.class, args);
+	}
+
+}
